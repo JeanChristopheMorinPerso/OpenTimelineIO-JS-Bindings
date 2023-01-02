ba11858858782ef4755c2bddf415a7390c758cb0
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2023-01-02 15:18:15 -0500

    Implement TimeTransform and TimeRange
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

--- src/opentime/bindings.cpp
+++ src/opentime/bindings.cpp
@@ -29,6 +29,15 @@ string_printf(char const* format, Args... args)
     return std::string(buf.get());
 }
 
+std::string
+opentime_js_string(RationalTime rt)
+{
+    return string_printf(
+        "RationalTime(value=%g, rate=%g)",
+        rt.value(),
+        rt.rate());
+}
+
 EMSCRIPTEN_BINDINGS(opentime)
 {
 
@@ -39,12 +48,7 @@ EMSCRIPTEN_BINDINGS(opentime)
         .function("is_invalid_time", &RationalTime::is_invalid_time)
         .property("value", &RationalTime::value)
         .property("rate", &RationalTime::rate)
-        .function("toString", ems::optional_override([](RationalTime& rt) {
-                      return string_printf(
-                          "RationalTime(value=%g, rate=%g)",
-                          rt.value(),
-                          rt.rate());
-                  }))
+        .function("toString", &opentime_js_string)
         .function("valueOf", ems::optional_override([](RationalTime& rt) {
                       // This is what RationalTime class does internally when comparing
                       // rational times.
@@ -125,7 +129,111 @@ EMSCRIPTEN_BINDINGS(opentime)
                     return RationalTime::from_time_string(timecode, rate);
                 }));
 
-    ems::class_<TimeRange>("TimeRange").constructor<RationalTime>();
+    ems::class_<TimeRange>("TimeRange")
+        // TODO: Match Python constructor logic?
+        .constructor()
+        .constructor<RationalTime>()
+        .constructor<RationalTime, RationalTime>()
+        .property("start_time", &TimeRange::start_time)
+        .property("duration", &TimeRange::duration)
+        .function("toString", ems::optional_override([](TimeRange& tr) {
+                      return string_printf(
+                          "TimeRange(start_time=%s, end_time=%s)",
+                          opentime_js_string(tr.start_time()).c_str(),
+                          opentime_js_string(tr.duration()).c_str());
+                  }))
+        // TODO: Implement valueOf and toPrimitive to support comparisons
+        // .function("valueOf", ems::optional_override([](TimeRange& rt) {
+        //               todo
+        //           }))
+        // .function(
+        //     "toPrimitive",
+        //     ems::optional_override([](TimeRange& rt, std::string hint) {
+        //         todo
+        //     }))
+        .function("end_time_inclusive", &TimeRange::end_time_inclusive)
+        .function("duration_extended_by", &TimeRange::duration_extended_by)
+        .function("extended_by", &TimeRange::extended_by)
+        .function(
+            "clamped",
+            ems::select_overload<RationalTime(RationalTime) const>(
+                &TimeRange::clamped))
+        .function(
+            "clamped",
+            ems::select_overload<TimeRange(TimeRange) const>(
+                &TimeRange::clamped))
+        .function(
+            "contains",
+            ems::select_overload<bool(RationalTime) const>(
+                &TimeRange::contains))
+        .function(
+            "contains",
+            ems::select_overload<bool(TimeRange, double) const>(
+                &TimeRange::contains))
+        .function(
+            "overlaps",
+            ems::select_overload<bool(RationalTime) const>(
+                &TimeRange::overlaps))
+        .function(
+            "overlaps",
+            ems::select_overload<bool(TimeRange, double) const>(
+                &TimeRange::overlaps))
+        .function(
+            "before",
+            ems::select_overload<bool(RationalTime, double) const>(
+                &TimeRange::before))
+        .function(
+            "before",
+            ems::select_overload<bool(TimeRange, double) const>(
+                &TimeRange::before))
+        .function("meets", &TimeRange::meets)
+        .function(
+            "begins",
+            ems::select_overload<bool(RationalTime, double) const>(
+                &TimeRange::begins))
+        .function(
+            "begins",
+            ems::select_overload<bool(TimeRange, double) const>(
+                &TimeRange::begins))
+        .function(
+            "finishes",
+            ems::select_overload<bool(RationalTime, double) const>(
+                &TimeRange::finishes))
+        .function(
+            "finishes",
+            ems::select_overload<bool(TimeRange, double) const>(
+                &TimeRange::finishes))
+        .function("intersects", &TimeRange::intersects)
+        .class_function(
+            "range_from_start_end_time",
+            &TimeRange::range_from_start_end_time)
+        .class_function(
+            "range_from_start_end_time_inclusive",
+            &TimeRange::range_from_start_end_time_inclusive);
 
-    ems::class_<TimeTransform>("TimeTransform");
+    ems::class_<TimeTransform>("TimeTransform")
+        .constructor<RationalTime, double, double>()
+        .property("offset", &TimeTransform::offset)
+        .property("scale", &TimeTransform::scale)
+        .property("rate", &TimeTransform::rate)
+        .function("toString", ems::optional_override([](TimeTransform& tt) {
+                      return string_printf(
+                          "TimeTransform(offset=%s, scale=%g, rate=%g)",
+                          opentime_js_string(tt.offset()).c_str(),
+                          tt.scale(),
+                          tt.rate());
+                  }))
+        // TODO: Implement valueOf and toPrimitive to support comparisons
+        .function(
+            "applied_to",
+            ems::select_overload<TimeRange(TimeRange) const>(
+                &TimeTransform::applied_to))
+        .function(
+            "applied_to",
+            ems::select_overload<TimeTransform(TimeTransform) const>(
+                &TimeTransform::applied_to))
+        .function(
+            "applied_to",
+            ems::select_overload<RationalTime(RationalTime) const>(
+                &TimeTransform::applied_to));
 }
