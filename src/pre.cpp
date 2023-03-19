ed85f272c3ed23185cb0d6e6ca74bf28a2a6ffa4
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2023-03-19 15:49:43 -0400

    Add empty target to let CMake re-link the libraries when pre.js changes
    Signed-off-by: Jean-Christophe Morin <jean_christophe_morin@hotmail.com>

---
+++ src/pre.cpp
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Contributors to the OpenTimelineIO project
+
+// This file only exists to help CMake know when to re-link. In our case, when pre.js changes.
+// See https://blog.squareys.de/cmake-relink-js/.
